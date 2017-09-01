0fbb5ed9090b6db8b18828af45a6c4dfd96afbdb
Jason Kirby <jasonk@vdsys.com>
2017-09-01 10:09:40 -0600

    Tied in Firebase backend to authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  ngOnInit(){
+    firebase.initializeApp({
+        apiKey: "AIzaSyD2ptAU8T9tvlLpT2PeZNOuSR3BjRhU6pA",
+        authDomain: "ng-recipe-book-7eb0f.firebaseapp.com"
+    });
+  }
 }

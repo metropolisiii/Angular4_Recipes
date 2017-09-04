be5393f6a9e58515871a279624bf1bf1148a68a5
Jason Kirby <jasonk@vdsys.com>
2017-09-04 09:44:37 -0600

    Created an action and dispatched it for adding ingredients to the shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -10,19 +10,11 @@ export class ShoppingListService{
     new Ingredient('Tomatoes', 10),    
   ];
   
-  getIngredients(){
-    return this.ingredients.slice();
-  }
-  
+   
   getIngredient(index: number){
     return this.ingredients[index];
   }
-  
-  addIngredient(ingredient:Ingredient){
-    this.ingredients.push(ingredient);
-    this.ingredientsChanged.next(this.ingredients.slice());
-  }
-  
+   
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -4,9 +4,10 @@ import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
-export class AddIngrient implements Action {
+export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
-    payload: Ingredient;
+    
+    constructor(public payload: Ingredient){}
 }
 
-export type ShoppingListActions = AddIngrient;
+export type ShoppingListActions = AddIngredient;

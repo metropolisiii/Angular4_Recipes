7d60020d39dfbbc60ed53b39e0bd37f695bba49d
Jason Kirby <jasonk@vdsys.com>
2017-09-04 10:51:21 -0600

    Convert delete and update shopping list to rxjs

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,9 +3,9 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { Recipe } from '../recipe.model';
-import { Ingredient } from '../../shared/ingredient.model';
 import { RecipeService} from '../recipe.service';
 import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
+import * as fromShoppingList from '../../shopping-list/store/shopping-list.reducers';
 
 
 @Component({
@@ -20,7 +20,7 @@ export class RecipeDetailComponent implements OnInit {
   constructor(private recipeService: RecipeService, 
               private route: ActivatedRoute,
               private router: Router,
-              private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
+              private store: Store<fromShoppingList.AppState>) { }
 
   ngOnInit() {
     this.route.params
